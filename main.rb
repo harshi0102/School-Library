4da3977a7c604cb8a075d9d24ef2845e822deac7
unknown <harshika0102@gmail.com>
2023-06-26 19:49:06 +0530

    Fixed Linters

--- main.rb
+++ main.rb
@@ -3,16 +3,16 @@ require_relative 'student'
 require_relative 'teacher'
 
 person = Person.new(16)
-puts person.can_use_services?  # Output: true
+puts person.can_use_services? # Output: true
 
-person.name = "John"
+person.name = 'John'
 person.age = 20
-puts person.name  # Output: John
-puts person.age   # Output: 20
+puts person.name # Output: John
+puts person.age # Output: 20
 
-student = Student.new(15, "Math")
-puts student.can_use_services?  # Output: true
-puts student.play_hooky          # Output: ¯\(ツ)/¯
+student = Student.new(15, 'Math')
+puts student.can_use_services? # Output: true
+puts student.play_hooky # Output: ¯\(ツ)/¯
 
-teacher = Teacher.new(35, "Physics", "Jane")
-puts teacher.can_use_services?  # Output: true
+teacher = Teacher.new(35, 'Physics', 'Jane')
+puts teacher.can_use_services? # Output: true
--- person.rb
+++ person.rb
@@ -1,29 +1,21 @@
 class Person
-    attr_reader :id, :name, :age
-  
-    def initialize(age, name = 'Unknown', parent_permission: true)
-      @id = Random.rand(1..100)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
-    end
-  
-    def name=(new_name)
-      @name = new_name
-    end
-  
-    def age=(new_age)
-      @age = new_age
-    end
-  
-    def can_use_services?
-      of_age? || @parent_permission
-    end
-  
-    private
-  
-    def of_age?
-      @age >= 18
-    end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
-  
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
